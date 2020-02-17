a1f07cf3753a8c30e59b02fa5cc27b0ade992eb6
rottbers <johan@rottbers.com>
2020-02-17 16:18:22 +0100

    Initial firestore integration to add orgs and users

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,5 +1,6 @@
 import app from 'firebase/app';
 import 'firebase/auth';
+import 'firebase/firestore';
 
 const config = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -10,18 +11,56 @@ const config = {
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
 };
 
+// make firebase object available in console during development
+if (process.env.NODE_ENV === 'development') {
+  window.firebase = app;
+}
+
 class Firebase {
   constructor() {
     app.initializeApp(config);
     this.auth = app.auth();
+    this.firestore = app.firestore();
   }
-  /*   Auth API */
+
+  // ##########
+  // Auth API
+  // ##########
+
   doCreateUserWithEmailAndPassword = (email, password) =>
     this.auth.createUserWithEmailAndPassword(email, password);
+
   doSignInWithEmailAndPassword = (email, password) =>
     this.auth.signInWithEmailAndPassword(email, password);
+
   doSignOut = () => this.auth.signOut();
   doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
   doPasswordUpdate = password => this.auth.currentUser.updatePassword(password);
+
+  // ##########
+  // Firestore API
+  // ##########
+
+  users = () => this.firestore.collection('users');
+  user = userId => this.firestore.collection('users').doc(userId);
+
+  organizations = () => this.firestore.collection('organization');
+  organization = orgId => this.firestore.collection('organization').doc(orgId);
+
+  createOrganization = ({ name, userId }) =>
+    this.firestore.collection('organization').add({ name, users: [userId] });
+
+  // TODO: updateOrganization
+
+  createUser = ({ userId, email, orgId, ...rest }) =>
+    this.firestore
+      .collection('users')
+      .doc(userId)
+      .set({ email, orgId, ...rest });
+
+  // TODO: updateUser
+
+  // TODO: deleteUser
 }
+
 export default Firebase;
