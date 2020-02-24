4b56524bfd35e3f3e40a9129f24c0479e0735eb6
Joakim Roos <joakim.roos@student.kyh.se>
2020-02-24 17:41:28 +0100

    Birds Animation for landing page

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,16 +1,55 @@
-import React from 'react';
-/* import Birds from './birds' */
+import React, { Component } from 'react';
 import styled, { ThemeProvider } from 'styled-components';
-import { Theme } from '../GlobalStyles';
+import BIRDS from './birds';
 
-const Landing = () => {
-  return (
-    <ThemeProvider theme={Theme}>
-      <div>
-        {/* <Birds /> */}
-      </div>
-    </ThemeProvider>
-  )
+const Background = styled.div`
+width: 100vw;
+height: 100vh;
+position: absolute;
+z-index: -1000;
+`;
+
+const Intro = styled.h1`
+margin-left: 15rem;
+text-align: left;
+line-height: 80vh;
+font-size: 60px;
+`;
+class Landing extends Component {
+  constructor() {
+    super()
+    this.birdAnimation = React.createRef();
+  }
+  componentDidMount() {
+    this.vantaEffect = BIRDS({
+      el: this.birdAnimation.current,
+      mouseControls: true,
+      touchControls: true,
+      minHeight: 200.00,
+      minWidth: 200.00,
+      scale: 1.00,
+      scaleMobile: 1.00,
+      backgroundColor: 0xd9d9d9,
+      color1: 0x200808,
+      color2: 0x8181b,
+      wingSpan: 40.00,
+      speedLimit: 4.00,
+      quantity: 3.00
+    })
+  }
+  componentWillUnmount() {
+    if (this.vantaEffect) this.vantaEffect.destroy()
+  }
+  render() {
+    return (
+      <Background className='birds' ref={this.birdAnimation}>
+
+        <Intro>Birds Eye View</Intro>
+      </Background >
+    )
+  }
 }
 
 export default Landing;
+
+
