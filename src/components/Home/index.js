56d8c663ed9dbe38b078b0362f5c7e4d113fa1b5
rottbers <johan@rottbers.com>
2020-02-24 13:30:47 +0100

    Removed victory chart

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Charts from '../Charts';
-import VictoryChart from '../VictoryChart';
 import { ScreenBadge } from 'react-awesome-styled-grid';
 import styled, { ThemeProvider } from 'styled-components';
 
@@ -8,24 +7,24 @@ import { withAuthorization } from '../Session';
 import { Theme } from '../GlobalStyles';
 
 const Layout = styled.div`
-	display: grid;
-	grid-template-columns: repeat(10, 1fr);
-	grid-auto-rows: 1fr;
-	/* 	//grid-template-columns: repeat(auto-fill, minmax(28em, 2fr)); */
-	grid-gap: 10px;
+  display: grid;
+  grid-template-columns: repeat(10, 1fr);
+  grid-auto-rows: 1fr;
+  /* 	//grid-template-columns: repeat(auto-fill, minmax(28em, 2fr)); */
+  grid-gap: 10px;
 `;
 
 const HomePage = () => {
-	return (
-		<ThemeProvider theme={Theme}>
-			<Layout>
-				<Charts />
-				<Charts />
-				{/* 	<ScreenBadge /> */}
-			</Layout>
-		</ThemeProvider>
-	);
+  return (
+    <ThemeProvider theme={Theme}>
+      <Layout>
+        <Charts />
+        <Charts />
+        {/* 	<ScreenBadge /> */}
+      </Layout>
+    </ThemeProvider>
+  );
 };
 
-const condition = (authUser) => !!authUser;
+const condition = authUser => !!authUser;
 export default withAuthorization(condition)(HomePage);
