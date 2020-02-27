b702d55bdb61dd530cf2738b7a3d51de79be4515
martin-linden <martin.linden@student.kyh.se>
2020-02-27 13:06:06 +0100

    sign in card is mostly done, minor bugs im fixing later

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -15,7 +15,7 @@ import * as Styled from './styled';
 
 const GlobalStyle = createGlobalStyle`
 body {
-  background-color: ${props => props.theme.bgPrimary};
+  background-color: ${(props) => props.theme.bgPrimary};
   padding: 0;
   margin: 0;
   font-family: 'Open Sans', sans-serif;
@@ -27,34 +27,50 @@ body {
 }`;
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      authUser: null
-    };
-  }
-  render() {
-    return (
-      <ThemeProvider theme={Theme}>
-        <Router>
-          <GlobalStyle bg />
-          <Styled.GridLayout>
-            <Navigation authUser={this.state.authUser} />
-            <Route exact path={ROUTES.LANDING} component={LandingPage} />
-            <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
-            <Route path={ROUTES.SIGN_IN} component={SignInPage} />
-            <Route
-              path={ROUTES.PASSWORD_FORGET}
-              component={PasswordForgetPage}
-            />
-            <Route path={ROUTES.DASHBOARD} component={DashboardPage} />
-            <Route path={ROUTES.ACCOUNT} component={AccountPage} />
-          </Styled.GridLayout>
-        </Router>
-      </ThemeProvider>
-    );
-  }
+		this.state = {
+			authUser: null
+		};
+	}
+	render() {
+		return (
+			<ThemeProvider theme={Theme}>
+				<Router>
+					<GlobalStyle bg />
+					<Styled.GridLayout>
+						<Navigation authUser={this.state.authUser} />
+						<Route
+							exact
+							path={ROUTES.LANDING}
+							component={LandingPage}
+						/>
+						<Route
+							path={ROUTES.SIGN_UP}
+							component={SignUpPage}
+						/>
+						<Route
+							path={ROUTES.SIGN_IN}
+							component={SignInPage}
+						/>
+						<Route
+							path={ROUTES.PASSWORD_FORGET}
+							component={PasswordForgetPage}
+						/>
+						<Route
+							path={ROUTES.DASHBOARD}
+							component={DashboardPage}
+						/>
+						<Route
+							path={ROUTES.ACCOUNT}
+							component={AccountPage}
+						/>
+					</Styled.GridLayout>
+				</Router>
+			</ThemeProvider>
+		);
+	}
 }
 
 export default withAuthentication(App);
--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -6,112 +6,120 @@ import * as ROUTES from '../../constants/routes';
 import * as Styled from './styled';
 
 const SignUpPage = () => (
-  <Styled.Grid>
-    <Styled.Wrapper>
-      <h1>Sign up</h1>
-      <SignUpForm />
-    </Styled.Wrapper>
-  </Styled.Grid>
+	<Styled.Grid>
+		<Styled.Wrapper>
+			<h1>Sign up</h1>
+			<SignUpForm />
+		</Styled.Wrapper>
+	</Styled.Grid>
 );
 const INITIAL_STATE = {
-  name: '',
-  email: '',
-  passwordOne: '',
-  passwordTwo: '',
-  error: null
+	name: '',
+	email: '',
+	passwordOne: '',
+	passwordTwo: '',
+	error: null
 };
 class SignUpFormBase extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { ...INITIAL_STATE };
-  }
+	constructor(props) {
+		super(props);
+		this.state = { ...INITIAL_STATE };
+	}
 
-  onSubmit = event => {
-    const { name, email, passwordOne } = this.state;
+	onSubmit = (event) => {
+		const { name, email, passwordOne } = this.state;
 
-    this.props.firebase
-      .doCreateUserWithEmailAndPassword(email, passwordOne)
-      .then(authUser => {
-        const { uid, email } = authUser.user;
+		this.props.firebase
+			.doCreateUserWithEmailAndPassword(email, passwordOne)
+			.then((authUser) => {
+				const { uid, email } = authUser.user;
 
-        // create org in 'organizations' collection and add user ID
-        this.props.firebase
-          .organizations()
-          .add({ name, users: [uid] })
-          .then(org => {
-            // create user in 'users' collection as admin and add org ID
-            this.props.firebase.user(uid).set(
-              {
-                email,
-                orgId: org.id,
-                role: 'Admin',
-                settings: { color: '', logo: '' }
-              },
-              { merge: true }
-            );
-          });
+				// create org in 'organizations' collection and add user ID
+				this.props.firebase
+					.organizations()
+					.add({ name, users: [ uid ] })
+					.then((org) => {
+						// create user in 'users' collection as admin and add org ID
+						this.props.firebase.user(uid).set(
+							{
+								email,
+								orgId: org.id,
+								role: 'Admin',
+								settings: { color: '', logo: '' }
+							},
+							{ merge: true }
+						);
+					});
 
-        this.setState({ ...INITIAL_STATE });
-        this.props.history.push(ROUTES.DASHBOARD);
-      })
-      .catch(error => {
-        this.setState({ error });
-      });
-    event.preventDefault();
-  };
-  onChange = event => {
-    this.setState({ [event.target.name]: event.target.value });
-  };
-  onChangeCheckbox = event => {
-    this.setState({ [event.target.name]: event.target.checked });
-  };
-  render() {
-    const { name, email, passwordOne, passwordTwo, error } = this.state;
+				this.setState({ ...INITIAL_STATE });
+				this.props.history.push(ROUTES.DASHBOARD);
+			})
+			.catch((error) => {
+				this.setState({ error });
+			});
+		event.preventDefault();
+	};
+	onChange = (event) => {
+		this.setState({ [event.target.name]: event.target.value });
+	};
+	onChangeCheckbox = (event) => {
+		this.setState({ [event.target.name]: event.target.checked });
+	};
+	render() {
+		const {
+			name,
+			email,
+			passwordOne,
+			passwordTwo,
+			error
+		} = this.state;
 
-    const isInvalid =
-      passwordOne !== passwordTwo || passwordOne === '' || name === '';
+		const isInvalid =
+			passwordOne !== passwordTwo ||
+			passwordOne === '' ||
+			name === '';
 
-    return (
-      <form onSubmit={this.onSubmit}>
-        <input
-          name="name"
-          onChange={this.onChange}
-          type="text"
-          placeholder="Organization name"
-        />
-        <input
-          name="email"
-          value={email}
-          onChange={this.onChange}
-          type="text"
-          placeholder="Email Adress"
-        />
-        <input
-          name="passwordOne"
-          value={passwordOne}
-          onChange={this.onChange}
-          type="password"
-          placeholder="Password"
-        />{' '}
-        <input
-          name="passwordTwo"
-          value={passwordTwo}
-          onChange={this.onChange}
-          type="password"
-          placeholder="Confirm Password"
-        />
-        <button disabled={isInvalid} type="submit">
-          Sign Up
-        </button>
-        {error && <p>{error.message}</p>}
-      </form>
-    );
-  }
+		return (
+			<form onSubmit={this.onSubmit}>
+				<input
+					name="name"
+					onChange={this.onChange}
+					type="text"
+					placeholder="Organization name"
+				/>
+				<input
+					name="email"
+					value={email}
+					onChange={this.onChange}
+					type="text"
+					placeholder="Email Adress"
+				/>
+				<input
+					name="passwordOne"
+					value={passwordOne}
+					onChange={this.onChange}
+					type="password"
+					placeholder="Password"
+				/>{' '}
+				<input
+					name="passwordTwo"
+					value={passwordTwo}
+					onChange={this.onChange}
+					type="password"
+					placeholder="Confirm Password"
+				/>
+				<button disabled={isInvalid} type="submit">
+					Sign Up
+				</button>
+				{error && <p>{error.message}</p>}
+			</form>
+		);
+	}
 }
 const SignUpLink = () => (
-  <p>
-    Don't have an account? <Link to={ROUTES.SIGN_UP}> Sign Up</Link>
-  </p>
+	<p>
+		Don't have an account? <Link to={ROUTES.SIGN_UP}> Sign Up</Link>
+	</p>
 );
 
 const SignUpForm = compose(withRouter, withFirebase)(SignUpFormBase);
